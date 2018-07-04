c98b44c8fc0d103efdd9548ed4b2402df897b9ed
Andrey Pozdnyakov <Andrey.Pozdnyakov@cern.ch>
2018-07-04 17:13:37 +0200

    First commit: woring LT maker, fits and limit code. But the details need to be sorted out.

---
+++ makeLT.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+from ROOT import *
+import os,json
+gROOT.SetBatch()
+from SMHiggsSamples import *
+from utils import *
+
+import argparse
+parser =  argparse.ArgumentParser(description='Limit Tree maker')
+parser.add_argument("indir", help = "Input directory with flat trees.")
+parser.add_argument('-x', nargs='+', choices=['res', 'nonres'], required=True, default=None,
+                    help = "Choose which samlples to create the trees from.")
+parser.add_argument("-v", "--verbosity",  dest="verb", action="store_true", default=False,
+                    help="Print out more stuff")
+parser.add_argument("-l", "--lumi", dest="lumi", default=35.87,
+                    help="Integrated lumi to scale signal")
+parser.add_argument('-o', '--outDir', dest="outDir", type=str, default=None,
+                    required=True, help="Output directory (will be created).")
+
+opt = parser.parse_args()
+
+nodes = [ ["box", 50000], ["SM", 50000], [2, 49600], [3, 50000], [4, 50000], [5, 50000], [6, 50000],
+          [7, 50000], [8, 50000], [9, 49600], [10, 49800], [11, 50000], [12, 50000], [13, 50000] ]
+
+if opt.verb>0:
+  print SMHiggsNodes
+
+
+if __name__ == "__main__":
+  print "This is the __main__ part"
+
+  createDir(opt.outDir)
+  if 'nonres' in opt.x:
+
+    print "Doing signal"
+    for n in nodes:
+      if n[0]!="SM": continue # Only do SM node for now
+
+      fChain = TChain("bbggSelectionTree")
+      fname = opt.indir+"/output_GluGluToHHTo2B2G_node_"+str(n[0])+"_13TeV-madgraph.root"
+      fChain.Add(fname)
+      rootName = fname[fname.rfind('/')+1:]
+      if opt.verb: print rootName
+
+      outFileName = opt.outDir+"/LT_"+rootName
+
+      fChain.Process("bbggLTMaker.C+", "%.10f %s %i" % ( opt.lumi/n[1], outFileName, 0) )
+
+    print "Done with signal"
+
+    print "Doing Single Higgs samples"
+    for n in SMHiggsNodes:
+      if opt.verb: print n
+      fChain = TChain("bbggSelectionTree")
+      fname = opt.indir+'/'+n[0]
+      fChain.Add(fname)
+      outFileName = opt.outDir+"/LT_"+n[0]
+
+      fChain.Process("bbggLTMaker.C+", "%.10f %s %i" % ( opt.lumi*n[2]/n[1], outFileName, 1 ) )
+
+    os.system('hadd -f '+opt.outDir+'/LT_output_bbHToGG_M-125_13TeV_amcatnlo.root '+opt.outDir+'/LT_output_bbHToGG_M-125_4FS_yb*.root')
+
+    print "Done with Single Higgs"
+
+    print "Doing Data"
+    fChain = TChain("bbggSelectionTree")
+
+    fname = opt.indir+'/FakeData/DoubleEG.root'
+    fChain.Add(fname)
+    outFileName = opt.outDir+"/LT_DoubleEG.root"
+    
+    fChain.Process("bbggLTMaker.C+", "%f %s %i" % ( 1, outFileName, 0) )
+    
+    print "Done with Data"
+
+  print "Done with Main"
