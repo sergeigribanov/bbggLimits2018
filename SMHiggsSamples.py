ceb0418fb17ccecd3cc5c836d6c80b150cfe5a1c
lata <panwarlsweet@gmail.com>
2018-11-13 14:17:57 +0100

    solving error related 0 events in cat0 and removing addition param lines in the datacard templates, also removing ybyt sample for LT

--- SMHiggsSamples.py
+++ SMHiggsSamples.py
@@ -7,7 +7,7 @@ SMHiggsNodes = [
 ['output_GluGluHToGG_M-125_13TeV_powheg_pythia8.root', 998200.0, 48.5*1000.*hggbr*genDiPhoFilterFactor],
 ['output_VBFHToGG_M-125_13TeV_powheg_pythia8.root', 3738258.272705078, 3.748*1000.*hggbr*genDiPhoFilterFactor],
 ['output_VHToGG_M125_13TeV_amcatnloFXFX_madspin_pythia8.root', 1862202.21875, 2.257*1000*hggbr*genDiPhoFilterFactor],
-['output_ttHToGG_M125_13TeV_powheg_pythia8_v2.root', 431817.3866882324, 0.5071*1000*hggbr*genDiPhoFilterFactor],
+['output_ttHToGG_M125_13TeV_powheg_pythia8.root', 431817.3866882324, 0.5071*1000*hggbr*genDiPhoFilterFactor],
 ['output_bbHToGG_M-125_4FS_yb2_13TeV_amcatnlo.root', 297462.02209472656, 0.534403*1000*hggbr*genDiPhoFilterFactor],
-['output_bbHToGG_M-125_4FS_ybyt_13TeV_amcatnlo.root', -24582.49609375, 0.0464028*1000*hggbr*genDiPhoFilterFactor]
+#['output_bbHToGG_M-125_4FS_ybyt_13TeV_amcatnlo.root', -24582.49609375, 0.0464028*1000*hggbr*genDiPhoFilterFactor]
 ]
--- src/bbgg2DFitter.cc
+++ src/bbgg2DFitter.cc
@@ -94,7 +94,7 @@ RooArgSet* bbgg2DFitter::defineVariables(bool swithToSimpleWeight=false)
   }
 
 
-  for ( int i=1; i<=(_NCAT); ++i)
+  for ( int i=0; i<=(_NCAT); ++i)
     catID->defineType(Form("cat_%i", i), i);
   
   //
