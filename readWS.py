05c3595a4a493a1a6bf44d9e0a44883754dfe1eb
lata <panwarlsweet@gmail.com>
2018-12-08 18:46:40 +0100

    correcting macro to run on all the categories

--- readWS.py
+++ readWS.py
@@ -1,54 +1,54 @@
 #! /usr/bin/env python
 
-#import os,sys
+import os,sys
 import numpy as np
 
 from ROOT import *
 gROOT.SetBatch()
 
 # Read the workspaces:
-rooWs = TFile('LIMS_OutDir/Node_SM/ws_hhbbgg.HH.sig.mH125_13TeV.root')
+rooWs = TFile('LIMS_OutDir_Train_23_11_18/Node_SM/ws_hhbbgg.HH.sig.mH125_13TeV.root')
 
 myWs = rooWs.Get('w_all')
 myWs.Print()
 
 mgg = myWs.var('mgg')
 mjj = myWs.var('mjj')
+for i in range(0,12):
+  pdf_mgg  = myWs.pdf("mggSig_cat"+str(i)+"_CMS_sig_cat"+str(i))
+  pdf_mjj  = myWs.pdf("mjjSig_cat"+str(i)+"_CMS_sig_cat"+str(i))
+  pdf_prod = myWs.pdf("CMS_sig_cat"+str(i)) # This is a product of the two PDFs above
+  dataSet  = myWs.data("Sig_cat"+str(i))
 
-pdf_mgg  = myWs.pdf("mggSig_cat0_CMS_sig_cat0")
-pdf_mjj  = myWs.pdf("mjjSig_cat0_CMS_sig_cat0")
-pdf_prod = myWs.pdf("CMS_sig_cat0") # This is a product of the two PDFs above
-dataSet  = myWs.data("Sig_cat0")
-
-print "\n Printout the parameters of the sig PDF"
-l0 = RooArgSet(mgg,mjj)
-pars = pdf_prod.getParameters(l0)
-pars.Print()
-parsiter = pars.createIterator()
-var=parsiter.Next()
-while var!=None:
+  print "\n Printout the parameters of the sig PDF"
+  l0 = RooArgSet(mgg,mjj)
+  pars = pdf_prod.getParameters(l0)
+  pars.Print()
+  parsiter = pars.createIterator()
+  var=parsiter.Next()
+  while var!=None:
     print '%s: %.3f  +/ %.3f'%(var.GetName(), var.getVal(), var.getError())
     var=parsiter.Next()
 
 
-# Make some plots:
-print 
-c = TCanvas("c","c",0,0,900,600)
-c.cd()
+  # Make some plots:
+  print 
+  c = TCanvas("c","c",0,0,900,600)
+  c.cd()
 
-mgg.setRange('signal',118,135)
-mjj.setRange('signal', 70,190)
+  mgg.setRange('signal',118,135)
+  mjj.setRange('signal', 70,190)
 
-mggFrame = mgg.frame(RooFit.Range("signal"))
-dataSet.plotOn(mggFrame, RooFit.Binning(80))
-pdf_mgg.plotOn(mggFrame, RooFit.LineColor(kRed+1), RooFit.LineWidth(2))
-mggFrame.Draw()
-c.SaveAs('tmpfig_mgg.png')
+  mggFrame = mgg.frame(RooFit.Range("signal"))
+  dataSet.plotOn(mggFrame, RooFit.Binning(80))
+  pdf_mgg.plotOn(mggFrame, RooFit.LineColor(kRed+1), RooFit.LineWidth(2))
+  mggFrame.Draw()
+  c.SaveAs('tmpfig_mgg_cat'+str(i)+'.png')
 
-
-mjjFrame = mjj.frame(RooFit.Range("signal"))
-dataSet.plotOn(mjjFrame, RooFit.Binning(30))
-pdf_mjj.plotOn(mjjFrame, RooFit.LineColor(kGreen+1), RooFit.LineWidth(2))
-mjjFrame.Draw()
-c.SaveAs('tmpfig_mjj.png')
+ 
+  mjjFrame = mjj.frame(RooFit.Range("signal"))
+  dataSet.plotOn(mjjFrame, RooFit.Binning(30))
+  pdf_mjj.plotOn(mjjFrame, RooFit.LineColor(kGreen+1), RooFit.LineWidth(2))
+  mjjFrame.Draw()
+  c.SaveAs('tmpfig_mjj_cat'+str(i)+'.png')
 
