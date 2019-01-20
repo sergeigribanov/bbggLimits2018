01c47327b032d8a60c291dcd30f7e5a1cdb95320
lata <panwarlsweet@gmail.com>
2019-01-20 20:27:22 +0100

    script for ttH cut optimization

---
+++ scripts/tthCutOptimization.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+nodes="
+SM
+"
+
+num=0
+max=25
+cut=0.3
+bin=0.02
+
+for arg in $nodes:
+do
+    node=$arg
+    echo running limits for node $node
+    while [ $num -lt $max ]
+    do 
+    cut=`echo $cut + $bin | bc`
+
+    echo ttH_cut $cut
+
+    echo === step : creating workspace and datacards ===
+    ./runLimit.py -f conf_default_local.json --node=$node -o outDir_LIM_node_${node}_ttHcut_$num --analyticalRW --ttHTaggerCut=$cut
+
+    echo === step : running combine for limits ===
+    sh scripts/Analyzer.sh outDir_LIM_${node}_ttHcut$num    
+
+    num=$(( $num + 1 ))
+    done
+
+    echo done with node $node
+done
+ 
