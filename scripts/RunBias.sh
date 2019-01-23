c40f37519fcf6f488a66f6b94916ba027c078e1f
maxime <mgouzevi@cern.ch>
2019-01-23 10:56:13 +0100

    Updated bias script

--- scripts/RunBias.sh
+++ scripts/RunBias.sh
@@ -5,30 +5,45 @@ cd $LIMFOLDER
 mkdir BIAS
 mkdir BIAS/LOGS
 arr=(Ber Exp Pow)
+asig=(1 1 1 10 10 10 10 20 20 20 20 20)
 func=0
-NGEN=500
+NGEN=2000
 
 echo " ========== Func used " ${arr[func]} " with " ${NGEN} " events " 
 echo " "
 
-for icat in {0..3}
+for icat in {7..11}
 do
     for truth in {0..2}
     do
-	seed=$(($icat*100+$func+1)) 
-	DATACARD=hhbbgg_13TeV_DataCard_bias_cat${icat}.txt
-	PDFIDX=pdf_index_cat${icat}
-	TOYS=BIAS/higgsCombineTest.GenerateOnly.cat${icat}.tBer.f${arr[func]}.root
-	MLFIT=BIAS/mlfit_cat${icat}.tBer.f${arr[${func}]}.root
+	SIG=${asig[${icat}]}
+	seed=$(($icat*100+$func+$truth))
+	rMin=-10
+	rMax=$((2*$SIG+6))
 
-	echo "cat" ${icat} " with truth " ${arr[truth]} " fit by function " ${arr[func]} " with a random seed " ${seed}
-	combine $DATACARD -M GenerateOnly --setPhysicsModelParameters $PDFIDX=${truth} --toysFrequentist -t ${NGEN} --expectSignal 1 --saveToys -m ${icat} --freezeNuisances $PDFIDX -s ${seed} -v -1 &>BIAS/LOGS/Gen_cat${icat}.t${arr[${truth}]}.f${arr[${func}]}.txt
+	CAT=cat${icat}
+	DATACARD=hhbbgg_13TeV_DataCard_bias_${CAT}.txt
+	PDFIDX=pdf_index_$CAT
+	SETUP=${CAT}.t${arr[${truth}]}.f${arr[${func}]}.musig$SIG
+	GENERATE=higgsCombineTest.GenerateOnly.mH${icat}.${seed}.root
 
-	mv higgsCombineTest.GenerateOnly.mH${icat}.${seed}.root $TOYS
+
+	TOYS=BIAS/higgsCombineTest.GenerateOnly.${SETUP}.root
+	MLFIT=BIAS/mlfit.${SETUP}.root
+
+	echo $CAT " with truth " ${arr[truth]} " fit by function " ${arr[func]} " with a random seed " ${seed}
+	GENERATING="combine $DATACARD -M GenerateOnly --setPhysicsModelParameters $PDFIDX=${truth} --toysFrequentist -t ${NGEN} --expectSignal $SIG --saveToys -m ${icat} --freezeNuisances $PDFIDX -s ${seed} -v -1 &>BIAS/LOGS/Gen_${SETUP}.txt"
+	echo $GENERATING
+	eval $GENERATING
+
+	mv $GENERATE $TOYS
 	echo "generated " $TOYS
 
-	combine $DATACARD -M MaxLikelihoodFit  --setPhysicsModelParameters $PDFIDX=${func}  --toysFile $TOYS  -t ${NGEN} --rMin -5 --rMax 5  --freezeNuisances $PDFIDX -v -1 &>BIAS/LOGS/Fits_cat${icat}.t${arr[${truth}]}.f${arr[${func}]}.txt
+	MLFITTING="combine $DATACARD -M MaxLikelihoodFit  --setPhysicsModelParameters $PDFIDX=${func}  --toysFile $TOYS  -t ${NGEN} --rMin $rMin --rMax $rMax  --freezeNuisances $PDFIDX -v -1 &>BIAS/LOGS/MLFits_${SETUP}.txt"
+	echo ${MLFITTING}
+	eval ${MLFITTING}
 	mv mlfit.root $MLFIT
+	rm $TOYS
 	echo "fitted " $MLFIT
 	
     done
