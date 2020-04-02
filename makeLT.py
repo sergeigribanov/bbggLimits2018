3c45660ae938d5f789fc0d9f0103451ba2dd2f30
ivovtin <ivan.ovtin@cern.ch>
2020-04-02 06:15:26 +0200

    13 cats for VBFHH

--- makeLT.py
+++ makeLT.py
@@ -46,7 +46,8 @@ if __name__ == "__main__":
 #      fChain = TChain("tagsDumper/trees/bbggSelectionTree")
       fChain = TChain("bbggSelectionTree")
       #fname = opt.indir+"/output_GluGluToHHTo2B2G_node_"+str(n[0])+"_13TeV-madgraph.root"  
-      fname = opt.indir+"/output_GluGluToHHTo2B2G_allnodes_no_unit_norm.root"
+      #fname = opt.indir+"/output_GluGluToHHTo2B2G_allnodes_no_unit_norm.root"
+      fname = opt.indir+"/output_GluGluToHHTo2B2G_all_nodes.root"
       fChain.Add(fname)
       ttHkiller = fChain.GetListOfBranches().FindObject("ttHScore");
       if ttHkiller: _ttHTagger=1
