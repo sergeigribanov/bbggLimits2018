efd96c294714838bc3562cc8d0952976e4835e48
ivovtin <ivan.ovtin@cern.ch>
2020-04-28 15:15:34 +0200

    28042020_update

---
+++ runvbfhhLT.sh
@@ -0,0 +1,4 @@
+#!/bin/bash/
+
+./makeLT.py /afs/cern.ch/work/i/ivovtin/public/legacy_branch_flattrees/vbfhh/2018/withMVA/ -x nonres -l 59.4 -o /afs/cern.ch/work/i/ivovtin/Hggbb/LT_vbfhh_data_13cat -c 6
+#./makeLT.py /afs/cern.ch/work/i/ivovtin/public/legacy_branch_flattrees/vbfhh/2018/withMVA/ -x nonres -l 59.4 -o /afs/cern.ch/work/i/ivovtin/Hggbb/LT_vbfhh_gghh_data_12cat -c 4
