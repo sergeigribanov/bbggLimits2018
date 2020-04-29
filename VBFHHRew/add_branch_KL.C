80011c1f8bb02cdb4b3223dffa60fe7c73187c70
ivovtin <ivan.ovtin@cern.ch>
2020-04-29 16:40:51 +0200

    updateKL

--- VBFHHRew/add_branch_KL.C
+++ VBFHHRew/add_branch_KL.C
@@ -30,7 +30,7 @@ void add_branch_KL()
       char branchname[81];     
       float KL[81], C2V[81];
       TBranch *brKL;     
-      TBranch *brC2V;     
+      TBranch *brC2V[81];     
       
       if ( KLscan )
       {
@@ -45,33 +45,35 @@ void add_branch_KL()
          for (int n = 0; n < 81; n++) 
          {
            sprintf(branchname,"C2V%d",n+1);
-           brC2V = newtree->Branch(branchname, &C2V[n]); 
+           brC2V[n] = newtree->Branch(branchname, &C2V[n]); 
          }
       }
-    
-      float inKL=0, inC2V=0, inCV=0,  weight1=0, weight2=0, weight3=0, weight4=0, weight5=0, weight6=0; 
+     
       //ifstream KLweightfile ("outKLweight.txt");
       ifstream KLweightfile ("outC2Vweight.txt");
 
+      float inKL=0, inC2V=0, inCV=0,  weight1=0, weight2=0, weight3=0, weight4=0, weight5=0, weight6=0; 
       Long64_t nentries = newtree->GetEntries(); 
+      cout<<"nentries="<<nentries<<endl;
       for (Long64_t i=0; i<nentries; i++) 
       { 
-        newtree->GetEntry(i); 
- 
-        while(!KLweightfile.eof())
-        {
+        newtree->GetEntry(i);
+
+       while(!KLweightfile.eof())
+       {
             KLweightfile >> inKL >> inC2V >> inCV >> weight1 >> weight2 >> weight3 >> weight4 >> weight5 >> weight6;
             //cout<<"inKL="<<inKL<<"\t"<<"weight1="<<weight1<<endl; 
+            //cout<<i<<endl; 
             if ( KLscan )
             {   
               for(int k = 0; k < 81; k++)
               {
-                if( fname==Sig1 && (k+1)==inKL ) { KL[k]=weight1; cout<<"inKL="<<inKL<<"\t"<<"weight1="<<weight1<<endl; }
-                if( fname==Sig2 && (k+1)==inKL ) { KL[k]=weight2; cout<<"inKL="<<inKL<<"\t"<<"weight2="<<weight2<<endl; }
-                if( fname==Sig3 && (k+1)==inKL ) { KL[k]=weight3; cout<<"inKL="<<inKL<<"\t"<<"weight3="<<weight3<<endl; }
-                if( fname==Sig4 && (k+1)==inKL ) { KL[k]=weight4; cout<<"inKL="<<inKL<<"\t"<<"weight4="<<weight4<<endl; }
-                if( fname==Sig5 && (k+1)==inKL ) { KL[k]=weight5; cout<<"inKL="<<inKL<<"\t"<<"weight5="<<weight5<<endl; }
-                if( fname==Sig6 && (k+1)==inKL ) { KL[k]=weight6; cout<<"inKL="<<inKL<<"\t"<<"weight6="<<weight6<<endl; }
+                if( fname==Sig1 && (k+1)==inKL ) { KL[k]=weight1; /*cout<<"inKL="<<inKL<<"\t"<<"weight1="<<weight1<<endl;*/ }
+                if( fname==Sig2 && (k+1)==inKL ) { KL[k]=weight2; /*cout<<"inKL="<<inKL<<"\t"<<"weight2="<<weight2<<endl;*/ }
+                if( fname==Sig3 && (k+1)==inKL ) { KL[k]=weight3; /*cout<<"inKL="<<inKL<<"\t"<<"weight3="<<weight3<<endl;*/ }
+                if( fname==Sig4 && (k+1)==inKL ) { KL[k]=weight4; /*cout<<"inKL="<<inKL<<"\t"<<"weight4="<<weight4<<endl;*/ }
+                if( fname==Sig5 && (k+1)==inKL ) { KL[k]=weight5; /*cout<<"inKL="<<inKL<<"\t"<<"weight5="<<weight5<<endl;*/ }
+                if( fname==Sig6 && (k+1)==inKL ) { KL[k]=weight6; /*cout<<"inKL="<<inKL<<"\t"<<"weight6="<<weight6<<endl;*/ }
               }
             }
 
@@ -79,18 +81,19 @@ void add_branch_KL()
             {   
                 for(int k = 0; k < 81; k++)
               {
-                if( fname==Sig1 && (k+1)==inC2V ) { C2V[k]=weight1; cout<<k<<"\t"<<"inC2V="<<inC2V<<"\t"<<"weight1="<<weight1<<endl; }
-                if( fname==Sig2 && (k+1)==inC2V ) { C2V[k]=weight2; cout<<k<<"\t"<<"inC2V="<<inC2V<<"\t"<<"weight2="<<weight2<<endl; }
-                if( fname==Sig3 && (k+1)==inC2V ) { C2V[k]=weight3; cout<<k<<"\t"<<"inC2V="<<inC2V<<"\t"<<"weight3="<<weight3<<endl; }
-                if( fname==Sig4 && (k+1)==inC2V ) { C2V[k]=weight4; cout<<k<<"\t"<<"inC2V="<<inC2V<<"\t"<<"weight4="<<weight4<<endl; }
-                if( fname==Sig5 && (k+1)==inC2V ) { C2V[k]=weight5; cout<<k<<"\t"<<"inC2V="<<inC2V<<"\t"<<"weight5="<<weight5<<endl; }
-                if( fname==Sig6 && (k+1)==inC2V ) { C2V[k]=weight6; cout<<k<<"\t"<<"inC2V="<<inC2V<<"\t"<<"weight6="<<weight6<<endl; }
+                if( fname==Sig1 && (k+1)==inC2V ) { C2V[k]=weight1; /*cout<<k<<"\t"<<"inC2V="<<inC2V<<"\t"<<"weight1="<<weight1<<endl;*/ }
+                if( fname==Sig2 && (k+1)==inC2V ) { C2V[k]=weight2; /*cout<<k<<"\t"<<"inC2V="<<inC2V<<"\t"<<"weight2="<<weight2<<endl;*/ }
+                if( fname==Sig3 && (k+1)==inC2V ) { C2V[k]=weight3; /*cout<<k<<"\t"<<"inC2V="<<inC2V<<"\t"<<"weight3="<<weight3<<endl;*/ }
+                if( fname==Sig4 && (k+1)==inC2V ) { C2V[k]=weight4; /*cout<<k<<"\t"<<"inC2V="<<inC2V<<"\t"<<"weight4="<<weight4<<endl;*/ }
+                if( fname==Sig5 && (k+1)==inC2V ) { C2V[k]=weight5; /*cout<<k<<"\t"<<"inC2V="<<inC2V<<"\t"<<"weight5="<<weight5<<endl;*/ }
+                if( fname==Sig6 && (k+1)==inC2V ) { C2V[k]=weight6; /*cout<<k<<"\t"<<"inC2V="<<inC2V<<"\t"<<"weight6="<<weight6<<endl;*/ }
               }
             }
  
         }
-        
-        newtree->Fill();
+       
+        //newtree->Fill();
+        for (int n = 0; n < 81; n++) brC2V[n]->Fill();
       }   
       KLweightfile.close ();   
       newtree->Print();       
