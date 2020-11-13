54e9310464833a7f355ab3b1cf4bc0c675746ea0
Sergei Gribanov <ssgribanov@gmail.com>
2020-11-13 13:55:24 +0700

    applying changes to run2.sh

--- run2.sh
+++ run2.sh
@@ -1,6 +1,6 @@
 ##./runLimit.py -f jsons/conf_default_1D.json --node=SM -o ws_2016-17-18_ttHon0.26_1D_btagRew_19032020_withMjj_ourcat --overwrite --ttHTaggerCut 0.26
 ##./runLimit.py -f jsons/conf_default_2D.json --node=SM -o ws_2016-17-18_ttHon0.26_2D_Data_SigNLOkl5_18042020_woMjj_ptmjj0.55 --overwrite --ttHTaggerCut 0.26
-./runLimit.py -f jsons/conf_default_2D.json --node=SM -o ws_run2_ttHon0.26_2D_SM_ggHHonly_04062020 --overwrite --ttHTaggerCut 0.26
+./runLimit.py -f jsons/conf_default_2D.json --node=SM -o ws_run2_ttHon0.26_2D_SM_cHHH015_ggHHonly_NLO_04112020 --overwrite --ttHTaggerCut 0.26
 #./runLimit.py -f jsons/conf_default_2D_VBFHH.json --node=SM -o ws_2018_ttHon0.26_2D_MCbenriched_VBFHH_31032020_woMjj_ptmjj0.55 --overwrite --ttHTaggerCut 0.26
 #./runLimit.py -f jsons/conf_default_2D_VBFHH.json --node=SM -o ws_2018_ttHon0.26_2D_Data_VBFHH_09042020_woMjj_ptmjj0.55 --overwrite --ttHTaggerCut 0.26
 ##./runLimit.py -f jsons/conf_default_2D_15cat.json --node=SM -o ws_2016-17-18_ttHon0.26_2D_Data_15cat_20032020_woMjj_ptmjj0.5 --overwrite --ttHTaggerCut 0.26
