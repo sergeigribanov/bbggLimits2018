91466eda5e7b84d4bb19c48b74c9012485f031b3
Sergei Gribanov <ssgribanov@gmail.com>
2020-11-13 13:59:02 +0700

    applying changes to run_*.sh

--- runWSs_for_KL.sh
+++ runWSs_for_KL.sh
@@ -1,17 +1,13 @@
 #!/bin/bash/
 #list=( 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80)
-#list=( 1 6 11 16 17 18 21 24 26 28 31 34 35 36 37 38 39 41 44 46 47 48 51 53 56 61 66 71 76 80 )
-#list=( 1 6 11 16 17 18 21 26 31 34 35 37 38 41 44 47 48 51 56 61 66 71 76 80 )
-#list=( 36 ) 
-list=( 36 31 26 46 ) 
-#list=( 39 40 42 43 45 46 49 )
+list=( 26 31 46 ) 
 
 for ext in ${list[@]}
 do
   echo "Processing KL=" $ext
   sed -e 's/\(KL\)\([0-9][0-9]\)/\1'${ext}'/' conf_default_.json >conf_defaultKL${ext}.json
   sed -e 's/\(KL\)\([0-9][0-9]\)/\1'${ext}'/g' runWS_.sh >runWSKL${ext}.sh
-  #sed -e 's/\(KL\)\([0-9][0-9]\)/\1'${ext}'/g' condor_job_runWS_.sub >condor_job_runWSKL${ext}.sub
-  . runWSKL${ext}.sh
-  #condor_submit condor_job_runWSKL${ext}.sub 
+  sed -e 's/\(KL\)\([0-9][0-9]\)/\1'${ext}'/g' condor_job_runWS_.sub >condor_job_runWSKL${ext}.sub
+  #. runWSKL${ext}.sh
+  condor_submit condor_job_runWSKL${ext}.sub 
 done
